f08b16225ea841a548f138f9f64300fe65507e90
EvgenKh <hamitcevich@gmail.com>
2023-03-30 20:19:22 +0300

    add to app the pagination and router

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -11,7 +11,7 @@ function serializeResponse(movies) {
   }, {});
 };
 
-const { MOVIES } = mutations;
+const { MOVIES, CURRENT_PAGE } = mutations;
 
 const moviesStore = {
   namespaced: true,
@@ -26,10 +26,14 @@ const moviesStore = {
     sliceIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
     currentPage: ({ currentPage }) => currentPage,
     moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+    moviesLength: ({ top250IDs }) => Object.keys(top250IDs).length,
   },
   mutations: {
     [MOVIES](state, value) {
       state.movies = value;
+    },
+    [CURRENT_PAGE](state, value) {
+      state.currentPage = value;
     }
   },
   actions: {
@@ -55,7 +59,12 @@ const moviesStore = {
         console.log(err);
       }
     },
+    changeCurrentPage({ commit, dispatch }, page) {
+      commit(CURRENT_PAGE, page);
+      dispatch("fetchMovies");
+    },
   },
+
 };
 
 export default moviesStore;
