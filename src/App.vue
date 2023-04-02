3b13345ce84f03bdd4d4cedcc703c02b87db085c
EvgenKh <hamitcevich@gmail.com>
2023-04-02 14:13:46 +0300

    add remote movies and search

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <LoaderAct />
     <PosterBg :poster="posterBg" />
+    <HeaderApp />
     <MoviesList :list="moviesList" @changePoster="onChangePoster" />
     <MoviesPagination
       :current-page="currentPage"
@@ -18,6 +19,7 @@ import MoviesList from "@/components/MoviesList";
 import PosterBg from "@/components/PosterBg";
 import MoviesPagination from "@/components/MoviesPagination";
 import LoaderAct from "@/components/LoaderAct";
+import HeaderApp from "@/components/HeaderApp";
 
 export default {
   name: "App",
@@ -26,6 +28,7 @@ export default {
     PosterBg,
     MoviesPagination,
     LoaderAct,
+    HeaderApp,
   },
   data: () => ({
     posterBg: "",
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -11,7 +11,7 @@ function serializeResponse(movies) {
   }, {});
 };
 
-const { MOVIES, CURRENT_PAGE } = mutations;
+const { MOVIES, CURRENT_PAGE, REMOVE_MOVIE, TODDLE_SEARCH } = mutations;
 
 const moviesStore = {
   namespaced: true,
@@ -20,6 +20,7 @@ const moviesStore = {
     moviesPerPage: 12,
     currentPage: 1,
     movies: {},
+    isSearch: false,
   },
   getters: {
     moviesList: ({ movies }) => movies,
@@ -27,6 +28,7 @@ const moviesStore = {
     currentPage: ({ currentPage }) => currentPage,
     moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
     moviesLength: ({ top250IDs }) => Object.keys(top250IDs).length,
+    isSearch: ({isSearch }) => isSearch,
   },
   mutations: {
     [MOVIES](state, value) {
@@ -34,6 +36,12 @@ const moviesStore = {
     },
     [CURRENT_PAGE](state, value) {
       state.currentPage = value;
+    },
+    [REMOVE_MOVIE](state, index) {
+      state.top250IDs.splice(index, 1);
+    },
+    [TODDLE_SEARCH](state, bool) {
+      state.isSearch = bool;
     }
   },
   actions: {
@@ -67,6 +75,36 @@ const moviesStore = {
       commit(CURRENT_PAGE, page);
       dispatch("fetchMovies");
     },
+    removeMovie({ commit, dispatch, state }, id) {
+      const index = state.top250IDs.findIndex(item => item === id);
+
+      if (index !== -1) {
+        commit(REMOVE_MOVIE, index);
+        dispatch("fetchMovies");
+      }
+    },
+    async searchMovies({ commit, dispatch }, query) {
+      try {
+        dispatch("toggelLoader", true, { root: true });
+
+        const response = await axios.get(`/?s=${query}`);
+
+        if(response.Error) {
+          throw Error(response.Error);
+        }
+
+        const movies = serializeResponse(response.Search);
+        commit(MOVIES, movies);
+
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        dispatch("toggelLoader", false, { root: true });
+      }
+    },
+    toggleSearchState({ commit }, bool){
+      commit(TODDLE_SEARCH, bool);
+    }
   },
 
 };
